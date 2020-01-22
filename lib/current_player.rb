80cc7330ac22eb2d46a400aea71ca26cae004bb2
confident-token-1019 <theoonetj@gmail.com>
2020-01-22 18:49:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,18 @@
 
 
-def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
+def count_turn(board)
+  plauer_turns = 0
+  board.each do |free_space|
+    if free_space == "X" || free_space == "O"
+     plauer_turns += 1
     end
   end
-  return turns
+  return plauer_turns
 end
 
 def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
+  plauer_turns = count_turn(board)
+  if plauer_turns % 2 == 0
     return "X"
   else
     return "O"
